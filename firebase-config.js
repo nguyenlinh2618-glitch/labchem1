0fcac9192d531080a763f8a17facae33d6618374
nguyenlinh2618-glitch <nguyenlinh2618@gmail.com>
2026-05-18 14:03:15 +0700

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.LABCHEM_FIREBASE = {
-  apiKey:            "AIzaSyCE9HRIyk5SovIW-3weshQVbRngoAw0Tzs",
-  authDomain:        "quan-li-hc-hsmd.firebaseapp.com",
-  databaseURL:       "https://quan-li-hc-hsmd-default-rtdb.firebaseio.com",
-  projectId:         "quan-li-hc-hsmd",
-  storageBucket:     "quan-li-hc-hsmd.firebasestorage.app",
-  messagingSenderId: "597985093503",
-  appId:             "1:597985093503:web:3fed2a9a804ffc25e61c4a"
-};
